92669c8407240c1aae99e401e3478bd07b54cbb7
Asadhashmi2002 <asadalihashmi2002@gmail.com>
2025-09-04 12:21:17 +0530

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -1,9 +1,12 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-
+import os
 # --- THIS IS THE LINE YOU MUST EDIT ---
 # Replace the text inside the quotes with your actual bot token.
 #token removed
+
+
+TOKEN = os.environ['TOKEN']
 # -------------------------------------
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
